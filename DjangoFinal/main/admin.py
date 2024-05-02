1d38c238a97de5cc6526ecd1584706ef50e0d313
anidanik <anidanik55@gmail.com>
2024-05-02 19:50:44 +0300

    Final

--- DjangoFinal/main/admin.py
+++ DjangoFinal/main/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-from .models import Games, Publisher, Mechanics,SerialNumber
-
-
-class GamesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'number_of_players', 'publisher')
-    list_display_links = ('name', 'publisher')
-    search_fields = ('name', 'number_of_players')
-
+from .models import Games, Publisher, Mechanics, SerialNumber
 
 admin.site.register(Publisher)
-admin.site.register(Games, GamesAdmin)
+admin.site.register(Games)
 admin.site.register(SerialNumber)
 admin.site.register(Mechanics)
